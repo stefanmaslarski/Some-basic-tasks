4dafc09318eec09ae5e5fd9fb51b5caed503acfb
stefan <stefan_maslarski@abv.bg>
2022-11-08 16:54:11 +0200

    one task

---
+++ Some_small_tasks_2022/Tests.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Tests {
+    public static void main(String[] args) {
+        System.out.println(0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1);
+        System.out.println(Integer.MAX_VALUE+1);
+
+
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.nextLine());
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.printf("%d " , j);
+            }
+            System.out.println();
+        }
+
+        for (int k = num-1; k >= 1; k--) {
+                for (int l = 1; l <= k; l++) {
+                    System.out.printf("%d " , l);
+                }
+            System.out.println();
+        }
+    }
+}
