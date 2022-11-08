9ca12a7c3bb84d1d0ec4d6424c401576456a245c
stefan <stefan_maslarski@abv.bg>
2022-11-08 16:56:35 +0200

    Class name update

--- Some_small_tasks_2022/Tests.java
+++ Some_small_tasks_2022/Tests.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Tests {
+public class TriangleNumbers {
     public static void main(String[] args) {
         System.out.println(0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1);
         System.out.println(Integer.MAX_VALUE+1);
